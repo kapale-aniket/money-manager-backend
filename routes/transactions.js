84230465f4497452fef0e8865a7aec24fb39d295
kapale-aniket <aniketkapale2002@gmail.com>
2026-02-06 19:01:47 +0530

    Add mongoose import for connection checks

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 const Transaction = require('../models/Transaction')
 const { startOfWeek, endOfWeek, startOfMonth, endOfMonth, startOfYear, endOfYear } = require('date-fns')
 
